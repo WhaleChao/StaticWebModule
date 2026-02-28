c1a6a943082e67eef61148773d48f91fd8c9c2db
whalechao <lumi63181107@gmail.com>
2026-02-28 16:12:27 +0800

    Add random mode (full random + same-subject random) with dark UI

--- exam_app/data.js
+++ exam_app/data.js
@@ -1,34 +1,422 @@
-const EXAM_DATA = {
-  "綜合法學（一）憲法、行政法、國際公法、國際私法": {
-    "code": "1A",
-    "files": {
-      "question": "102131_0201_綜合法學（一）憲法、.pdf",
-      "answer": "102131_ANS0201_綜合法學（一）憲法、.pdf",
-      "modification": "102131_MOD0201_綜合法學（一）憲法、.pdf"
-    }
+const EXAM_DATA = [
+  {
+    "year": 114,
+    "yearLabel": "114年",
+    "subjects": [
+      {
+        "subject": "綜合法學(一) 憲法、行政法、國際公法、國際私法",
+        "question": "114110_0101_綜合法學(一)(憲法.pdf",
+        "answer": "114110_ANS0101_綜合法學(一)(憲法.pdf",
+        "modification": null
+      },
+      {
+        "subject": "綜合法學(二) 民法、民事訴訟法",
+        "question": "114110_0201_綜合法學(二)(民法.pdf",
+        "answer": "114110_ANS0201_綜合法學(二)(民法.pdf",
+        "modification": null
+      },
+      {
+        "subject": "綜合法學(二) 公司法、保險法、票據法、海商法、證券交易法、法學英文",
+        "question": "114110_0202_綜合法學(二)(公司.pdf",
+        "answer": "114110_ANS0202_綜合法學(二)(公司.pdf",
+        "modification": null
+      },
+      {
+        "subject": "綜合法學(一) 刑法、刑事訴訟法、法律倫理",
+        "question": "114110_0301_綜合法學(一)(刑法.pdf",
+        "answer": "114110_ANS0301_綜合法學(一)(刑法.pdf",
+        "modification": null
+      }
+    ]
   },
-  "綜合法學（二）民法、民事訴訟法": {
-    "code": "2A",
-    "files": {
-      "question": "102131_0301_綜合法學（二）民法、.pdf",
-      "answer": "102131_ANS0301_綜合法學（二）民法、.pdf",
-      "modification": "102131_MOD0301_綜合法學（二）民法、.pdf"
-    }
+  {
+    "year": 113,
+    "yearLabel": "113年",
+    "subjects": [
+      {
+        "subject": "綜合法學(一) 憲法、行政法、國際公法、國際私法",
+        "question": "113110_0101_綜合法學(一)(憲法.pdf",
+        "answer": "113110_ANS0101_綜合法學(一)(憲法.pdf",
+        "modification": null
+      },
+      {
+        "subject": "綜合法學(二) 民法、民事訴訟法",
+        "question": "113110_0201_綜合法學(二)(民法.pdf",
+        "answer": "113110_ANS0201_綜合法學(二)(民法.pdf",
+        "modification": null
+      },
+      {
+        "subject": "綜合法學(二) 公司法、保險法、票據法、海商法、證券交易法、法學英文",
+        "question": "113110_0202_綜合法學(二)(公司.pdf",
+        "answer": "113110_ANS0202_綜合法學(二)(公司.pdf",
+        "modification": null
+      },
+      {
+        "subject": "綜合法學(一) 刑法、刑事訴訟法、法律倫理",
+        "question": "113110_0301_綜合法學(一)(刑法.pdf",
+        "answer": "113110_ANS0301_綜合法學(一)(刑法.pdf",
+        "modification": null
+      }
+    ]
   },
-  "綜合法學（一）刑法、刑事訴訟法、法律倫理": {
-    "code": "3A",
-    "files": {
-      "question": "102131_0401_綜合法學（一）刑法、.pdf",
-      "answer": "102131_ANS0401_綜合法學（一）刑法、.pdf",
-      "modification": "102131_MOD0401_綜合法學（一）刑法、.pdf"
-    }
+  {
+    "year": 112,
+    "yearLabel": "112年",
+    "subjects": [
+      {
+        "subject": "綜合法學(一) 憲法、行政法、國際公法、國際私法",
+        "question": "112120_0101_綜合法學(一)(憲法.pdf",
+        "answer": "112120_ANS0101_綜合法學(一)(憲法.pdf",
+        "modification": null
+      },
+      {
+        "subject": "綜合法學(二) 民法、民事訴訟法",
+        "question": "112120_0201_綜合法學(二)(民法.pdf",
+        "answer": "112120_ANS0201_綜合法學(二)(民法.pdf",
+        "modification": null
+      },
+      {
+        "subject": "綜合法學(二) 公司法、保險法、票據法、海商法、證券交易法、法學英文",
+        "question": "112120_0202_綜合法學(二)(公司.pdf",
+        "answer": "112120_ANS0202_綜合法學(二)(公司.pdf",
+        "modification": null
+      },
+      {
+        "subject": "綜合法學(一) 刑法、刑事訴訟法、法律倫理",
+        "question": "112120_0301_綜合法學(一)(刑法.pdf",
+        "answer": "112120_ANS0301_綜合法學(一)(刑法.pdf",
+        "modification": null
+      }
+    ]
   },
-  "綜合法學（二）公司法、保險法、票據法、海商法、證券交易法、法學英文": {
-    "code": "4A",
-    "files": {
-      "question": "102131_0501_綜合法學（二）公司法.pdf",
-      "answer": "102131_ANS0501_綜合法學（二）公司法.pdf",
-      "modification": null
-    }
+  {
+    "year": 111,
+    "yearLabel": "111年",
+    "subjects": [
+      {
+        "subject": "綜合法學(一) 憲法、行政法、國際公法、國際私法",
+        "question": "111120_0101_綜合法學(一)(憲法.pdf",
+        "answer": "111120_ANS0101_綜合法學(一)(憲法.pdf",
+        "modification": "111120_MOD0101_綜合法學(一)(憲法.pdf"
+      },
+      {
+        "subject": "綜合法學(二) 民法、民事訴訟法",
+        "question": "111120_0201_綜合法學(二)(民法.pdf",
+        "answer": "111120_ANS0201_綜合法學(二)(民法.pdf",
+        "modification": null
+      },
+      {
+        "subject": "綜合法學(二) 公司法、保險法、票據法、海商法、證券交易法、法學英文",
+        "question": "111120_0202_綜合法學(二)(公司.pdf",
+        "answer": "111120_ANS0202_綜合法學(二)(公司.pdf",
+        "modification": null
+      },
+      {
+        "subject": "綜合法學(一) 刑法、刑事訴訟法、法律倫理",
+        "question": "111120_0301_綜合法學(一)(刑法.pdf",
+        "answer": "111120_ANS0301_綜合法學(一)(刑法.pdf",
+        "modification": null
+      }
+    ]
+  },
+  {
+    "year": 110,
+    "yearLabel": "110年",
+    "subjects": [
+      {
+        "subject": "綜合法學(一) 憲法、行政法、國際公法、國際私法",
+        "question": "110120_0101_綜合法學(一)(憲法.pdf",
+        "answer": "110120_ANS0101_綜合法學(一)(憲法.pdf",
+        "modification": "110120_MOD0101_綜合法學(一)(憲法.pdf"
+      },
+      {
+        "subject": "綜合法學(二) 民法、民事訴訟法",
+        "question": "110120_0201_綜合法學(二)(民法.pdf",
+        "answer": "110120_ANS0201_綜合法學(二)(民法.pdf",
+        "modification": null
+      },
+      {
+        "subject": "綜合法學(二) 公司法、保險法、票據法、海商法、證券交易法、法學英文",
+        "question": "110120_0202_綜合法學(二)(公司.pdf",
+        "answer": "110120_ANS0202_綜合法學(二)(公司.pdf",
+        "modification": null
+      },
+      {
+        "subject": "綜合法學(一) 刑法、刑事訴訟法、法律倫理",
+        "question": "110120_0301_綜合法學(一)(刑法.pdf",
+        "answer": "110120_ANS0301_綜合法學(一)(刑法.pdf",
+        "modification": "110120_MOD0301_綜合法學(一)(刑法.pdf"
+      }
+    ]
+  },
+  {
+    "year": 109,
+    "yearLabel": "109年",
+    "subjects": [
+      {
+        "subject": "綜合法學(一) 憲法、行政法、國際公法、國際私法",
+        "question": "109120_0101_綜合法學(一)(憲法.pdf",
+        "answer": "109120_ANS0101_綜合法學(一)(憲法.pdf",
+        "modification": null
+      },
+      {
+        "subject": "綜合法學(二) 民法、民事訴訟法",
+        "question": "109120_0201_綜合法學(二)(民法.pdf",
+        "answer": "109120_ANS0201_綜合法學(二)(民法.pdf",
+        "modification": null
+      },
+      {
+        "subject": "綜合法學(二) 公司法、保險法、票據法、海商法、證券交易法、法學英文",
+        "question": "109120_0202_綜合法學(二)(公司.pdf",
+        "answer": "109120_ANS0202_綜合法學(二)(公司.pdf",
+        "modification": null
+      },
+      {
+        "subject": "綜合法學(一) 刑法、刑事訴訟法、法律倫理",
+        "question": "109120_0301_綜合法學(一)(刑法.pdf",
+        "answer": "109120_ANS0301_綜合法學(一)(刑法.pdf",
+        "modification": "109120_MOD0301_綜合法學(一)(刑法.pdf"
+      }
+    ]
+  },
+  {
+    "year": 108,
+    "yearLabel": "108年",
+    "subjects": [
+      {
+        "subject": "綜合法學(一) 憲法、行政法、國際公法、國際私法",
+        "question": "108120_0101_綜合法學(一)(憲法.pdf",
+        "answer": "108120_ANS0101_綜合法學(一)(憲法.pdf",
+        "modification": "108120_MOD0101_綜合法學(一)(憲法.pdf"
+      },
+      {
+        "subject": "綜合法學(二) 民法、民事訴訟法",
+        "question": "108120_0201_綜合法學(二)(民法.pdf",
+        "answer": "108120_ANS0201_綜合法學(二)(民法.pdf",
+        "modification": null
+      },
+      {
+        "subject": "綜合法學(二) 公司法、保險法、票據法、海商法、證券交易法、法學英文",
+        "question": "108120_0202_綜合法學(二)(公司.pdf",
+        "answer": "108120_ANS0202_綜合法學(二)(公司.pdf",
+        "modification": null
+      },
+      {
+        "subject": "綜合法學(一) 刑法、刑事訴訟法、法律倫理",
+        "question": "108120_0301_綜合法學(一)(刑法.pdf",
+        "answer": "108120_ANS0301_綜合法學(一)(刑法.pdf",
+        "modification": null
+      }
+    ]
+  },
+  {
+    "year": 107,
+    "yearLabel": "107年",
+    "subjects": [
+      {
+        "subject": "綜合法學(一) 憲法、行政法、國際公法、國際私法",
+        "question": "107120_0101_綜合法學(一)(憲法.pdf",
+        "answer": "107120_ANS0101_綜合法學(一)(憲法.pdf",
+        "modification": "107120_MOD0101_綜合法學(一)(憲法.pdf"
+      },
+      {
+        "subject": "綜合法學(二) 民法、民事訴訟法",
+        "question": "107120_0201_綜合法學(二)(民法.pdf",
+        "answer": "107120_ANS0201_綜合法學(二)(民法.pdf",
+        "modification": null
+      },
+      {
+        "subject": "綜合法學(二) 公司法、保險法、票據法、海商法、證券交易法、法學英文",
+        "question": "107120_0202_綜合法學(二)(公司.pdf",
+        "answer": "107120_ANS0202_綜合法學(二)(公司.pdf",
+        "modification": "107120_MOD0202_綜合法學(二)(公司.pdf"
+      },
+      {
+        "subject": "綜合法學(一) 刑法、刑事訴訟法、法律倫理",
+        "question": "107120_0301_綜合法學(一)(刑法.pdf",
+        "answer": "107120_ANS0301_綜合法學(一)(刑法.pdf",
+        "modification": "107120_MOD0301_綜合法學(一)(刑法.pdf"
+      }
+    ]
+  },
+  {
+    "year": 106,
+    "yearLabel": "106年",
+    "subjects": [
+      {
+        "subject": "綜合法學(一) 憲法、行政法、國際公法、國際私法",
+        "question": "106120_0101_綜合法學(一)(憲法.pdf",
+        "answer": "106120_ANS0101_綜合法學(一)(憲法.pdf",
+        "modification": "106120_MOD0101_綜合法學(一)(憲法.pdf"
+      },
+      {
+        "subject": "綜合法學(二) 民法、民事訴訟法",
+        "question": "106120_0201_綜合法學(二)(民法.pdf",
+        "answer": "106120_ANS0201_綜合法學(二)(民法.pdf",
+        "modification": "106120_MOD0201_綜合法學(二)(民法.pdf"
+      },
+      {
+        "subject": "綜合法學(二) 公司法、保險法、票據法、海商法、證券交易法、法學英文",
+        "question": "106120_0202_綜合法學(二)(公司.pdf",
+        "answer": "106120_ANS0202_綜合法學(二)(公司.pdf",
+        "modification": null
+      },
+      {
+        "subject": "綜合法學(一) 刑法、刑事訴訟法、法律倫理",
+        "question": "106120_0301_綜合法學(一)(刑法.pdf",
+        "answer": "106120_ANS0301_綜合法學(一)(刑法.pdf",
+        "modification": "106120_MOD0301_綜合法學(一)(刑法.pdf"
+      }
+    ]
+  },
+  {
+    "year": 105,
+    "yearLabel": "105年",
+    "subjects": [
+      {
+        "subject": "綜合法學(一) 憲法、行政法、國際公法、國際私法",
+        "question": "105110_0101_綜合法學(一)(憲法.pdf",
+        "answer": "105110_ANS0101_綜合法學(一)(憲法.pdf",
+        "modification": null
+      },
+      {
+        "subject": "綜合法學(二) 民法、民事訴訟法",
+        "question": "105110_0201_綜合法學(二)(民法.pdf",
+        "answer": "105110_ANS0201_綜合法學(二)(民法.pdf",
+        "modification": "105110_MOD0201_綜合法學(二)(民法.pdf"
+      },
+      {
+        "subject": "綜合法學(二) 公司法、保險法、票據法、海商法、證券交易法、法學英文",
+        "question": "105110_0202_綜合法學(二)(公司.pdf",
+        "answer": "105110_ANS0202_綜合法學(二)(公司.pdf",
+        "modification": null
+      },
+      {
+        "subject": "綜合法學(一) 刑法、刑事訴訟法、法律倫理",
+        "question": "105110_0301_綜合法學(一)(刑法.pdf",
+        "answer": "105110_ANS0301_綜合法學(一)(刑法.pdf",
+        "modification": "105110_MOD0301_綜合法學(一)(刑法.pdf"
+      }
+    ]
+  },
+  {
+    "year": 104,
+    "yearLabel": "104年",
+    "subjects": [
+      {
+        "subject": "綜合法學(一) 憲法、行政法、國際公法、國際私法",
+        "question": "104110_0101_綜合法學(一)(憲法.pdf",
+        "answer": "104110_ANS0101_綜合法學(一)(憲法.pdf",
+        "modification": null
+      },
+      {
+        "subject": "綜合法學(二) 民法、民事訴訟法",
+        "question": "104110_0201_綜合法學(二)(民法.pdf",
+        "answer": "104110_ANS0201_綜合法學(二)(民法.pdf",
+        "modification": "104110_MOD0201_綜合法學(二)(民法.pdf"
+      },
+      {
+        "subject": "綜合法學(二) 公司法、保險法、票據法、海商法、證券交易法、法學英文",
+        "question": "104110_0202_綜合法學(二)(公司.pdf",
+        "answer": "104110_ANS0202_綜合法學(二)(公司.pdf",
+        "modification": null
+      },
+      {
+        "subject": "綜合法學(一) 刑法、刑事訴訟法、法律倫理",
+        "question": "104110_0301_綜合法學(一)(刑法.pdf",
+        "answer": "104110_ANS0301_綜合法學(一)(刑法.pdf",
+        "modification": "104110_MOD0301_綜合法學(一)(刑法.pdf"
+      }
+    ]
+  },
+  {
+    "year": 103,
+    "yearLabel": "103年",
+    "subjects": [
+      {
+        "subject": "綜合法學(一) 憲法、行政法、國際公法、國際私法",
+        "question": "103110_0101_綜合法學(一)(憲法.pdf",
+        "answer": "103110_ANS0101_綜合法學(一)(憲法.pdf",
+        "modification": "103110_MOD0101_綜合法學(一)(憲法.pdf"
+      },
+      {
+        "subject": "綜合法學(二) 民法、民事訴訟法",
+        "question": "103110_0201_綜合法學(二)(民法.pdf",
+        "answer": "103110_ANS0201_綜合法學(二)(民法.pdf",
+        "modification": "103110_MOD0201_綜合法學(二)(民法.pdf"
+      },
+      {
+        "subject": "綜合法學(二) 公司法、保險法、票據法、海商法、證券交易法、法學英文",
+        "question": "103110_0202_綜合法學(二)(公司.pdf",
+        "answer": "103110_ANS0202_綜合法學(二)(公司.pdf",
+        "modification": null
+      },
+      {
+        "subject": "綜合法學(一) 刑法、刑事訴訟法、法律倫理",
+        "question": "103110_0301_綜合法學(一)(刑法.pdf",
+        "answer": "103110_ANS0301_綜合法學(一)(刑法.pdf",
+        "modification": "103110_MOD0301_綜合法學(一)(刑法.pdf"
+      }
+    ]
+  },
+  {
+    "year": 102,
+    "yearLabel": "102年 (司法官)",
+    "subjects": [
+      {
+        "subject": "綜合法學(一) 憲法、行政法、國際公法、國際私法",
+        "question": "102121_0214_綜合法學(一)(憲法.pdf",
+        "answer": "102121_ANS0214_綜合法學(一)(憲法.pdf",
+        "modification": "102121_MOD0214_綜合法學(一)(憲法.pdf"
+      },
+      {
+        "subject": "綜合法學(一) 刑法、刑事訴訟法、法律倫理",
+        "question": "102121_0213_綜合法學(一)(刑法.pdf",
+        "answer": "102121_ANS0213_綜合法學(一)(刑法.pdf",
+        "modification": "102121_MOD0213_綜合法學(一)(刑法.pdf"
+      },
+      {
+        "subject": "綜合法學(二) 民法、民事訴訟法",
+        "question": "102121_0305_綜合法學(二)(民法.pdf",
+        "answer": "102121_ANS0305_綜合法學(二)(民法.pdf",
+        "modification": "102121_MOD0305_綜合法學(二)(民法.pdf"
+      },
+      {
+        "subject": "綜合法學(二) 公司法、保險法、票據法、海商法、證券交易法、法學英文",
+        "question": "102121_0109_綜合法學(二)(公司.pdf",
+        "answer": "102121_ANS0109_綜合法學(二)(公司.pdf",
+        "modification": "102121_MOD0109_綜合法學(二)(公司.pdf"
+      }
+    ]
+  },
+  {
+    "year": 102,
+    "yearLabel": "102年 (律師)",
+    "subjects": [
+      {
+        "subject": "綜合法學（一）憲法、行政法、國際公法、國際私法",
+        "question": "102131_0201_綜合法學（一）憲法、.pdf",
+        "answer": "102131_ANS0201_綜合法學（一）憲法、.pdf",
+        "modification": "102131_MOD0201_綜合法學（一）憲法、.pdf"
+      },
+      {
+        "subject": "綜合法學（二）民法、民事訴訟法",
+        "question": "102131_0301_綜合法學（二）民法、.pdf",
+        "answer": "102131_ANS0301_綜合法學（二）民法、.pdf",
+        "modification": "102131_MOD0301_綜合法學（二）民法、.pdf"
+      },
+      {
+        "subject": "綜合法學（一）刑法、刑事訴訟法、法律倫理",
+        "question": "102131_0401_綜合法學（一）刑法、.pdf",
+        "answer": "102131_ANS0401_綜合法學（一）刑法、.pdf",
+        "modification": "102131_MOD0401_綜合法學（一）刑法、.pdf"
+      },
+      {
+        "subject": "綜合法學（二）公司法、保險法、票據法、海商法、證券交易法、法學英文",
+        "question": "102131_0501_綜合法學（二）公司法.pdf",
+        "answer": "102131_ANS0501_綜合法學（二）公司法.pdf",
+        "modification": null
+      }
+    ]
   }
-};
+];
